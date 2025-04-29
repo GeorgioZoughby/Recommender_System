e5574b0b58f0c09331275486930a5a6fe474a7ac
Georgio Zoughby <georgio.zoghby@outlook.com>
2025-04-29 22:34:38 +0300

    Main File

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-# MAIN ENTRY OF THE PROGRAM WILL GO HERE
-
 from recommender import RecommenderSystem
 import pandas as pd
 import numpy as np
+
+# MAIN ENTRY OF THE PROGRAM WILL GO HERE
 if __name__ == "__main__":
-    data = {
-        'HP1': [4, 5, 2, 3],
-        'HP2': [5, 5, 4, 3],
-        'HP3': [1, 4, 5, np.nan],
-        'TW': [np.nan, np.nan, np. nan, np.nan],
-        'SW1': [np.nan, np.nan, np.nan, np.nan],
-        'SW2': [np.nan, np.nan, np.nan, np.nan],
-        'SW3': [np.nan, np.nan, np.nan, np.nan],
-    }
-    df = pd.DataFrame(data, index=['A', 'B', 'C', 'D'])
+    df = pd.read_csv("ratings.csv", index_col=0)
 
     recommender = RecommenderSystem()
     recommender.fit(df)
